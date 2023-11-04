f8275e421e2aaea999774838739fed9af0573d36
Marcio Curvello <marciocurvello@gmail.com>
2023-11-04 10:05:12 -0300

    add ScheduleAppointmentView

---
+++ Voll/Views/ScheduleAppointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleAppointmentView.swift
+//  Voll
+//
+//  Created by Marcio Curvello on 04/11/23.
+//
+
+import SwiftUI
+
+struct ScheduleAppointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleAppointmentView()
+}
