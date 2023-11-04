637b22bcc2e9ff8349cf1c667df51d939be1336a
Marcio Curvello <marciocurvello@gmail.com>
2023-11-04 10:40:46 -0300

    add extensions

---
+++ Voll/Extensions/Date+.swift
@@ -0,0 +1,8 @@
+//
+//  Date+.swift
+//  Voll
+//
+//  Created by Marcio Curvello on 04/11/23.
+//
+
+import Foundation
---
+++ Voll/Extensions/String+.swift
@@ -0,0 +1,8 @@
+//
+//  String+.swift
+//  Voll
+//
+//  Created by Marcio Curvello on 04/11/23.
+//
+
+import Foundation
