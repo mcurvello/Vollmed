c3affee0b24d244d23efac1de0824c9dc0e02cc0
Marcio Curvello <marciocurvello@gmail.com>
2023-11-04 01:29:16 -0300

    add getSpecialists

--- Voll/Services/WebService.swift
+++ Voll/Services/WebService.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 struct WebService {
-    private let baseUrl = "http://localhost:3000"
+    private let baseURL = "http://localhost:3000"
     
-    func getAllSpecialists() {
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let endpoint = baseURL + "/especialista"
         
+        guard let url = URL(string: endpoint) else {
+            print("Erro na URL!")
+            return nil
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
     }
 }
